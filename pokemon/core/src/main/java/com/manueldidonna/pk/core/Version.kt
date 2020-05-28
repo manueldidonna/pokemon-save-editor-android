bcf19b2d473553b4900f2d4280eb2eccfb88f507
manueldidonna <manueldidonnadev@gmail.com>
2020-05-28 21:05:44 +0200

    [core] Make Version a sealed class

--- pokemon/core/src/main/java/com/manueldidonna/pk/core/Version.kt
+++ pokemon/core/src/main/java/com/manueldidonna/pk/core/Version.kt
@@ -1,13 +1,13 @@
 package com.manueldidonna.pk.core
 
 /**
- * Version should be used primarily to retrieve different game resources.
+ * Version should be used primarily to retrieve the correct game resources.
  * The logic to manipulate the data shouldn't depends on this interface
  */
-interface Version {
-    val index: Int
+sealed class Version {
+    abstract val index: Int
 
-    data class FirstGeneration(val isYellow: Boolean) : Version {
+    data class FirstGeneration(val isYellow: Boolean) : Version() {
         override val index: Int = 0
     }
 }
