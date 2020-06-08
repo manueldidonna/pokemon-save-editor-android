6c8a58488e741e1e067d87460dcc8f38d96905df
manueldidonna <manueldidonnadev@gmail.com>
2020-06-08 02:21:06 +0200

    Add item names for english resources

--- pokemon/resources/src/main/java/com/manueldidonna/pk/resources/text/english/EnglishPokemonTextResources.kt
+++ pokemon/resources/src/main/java/com/manueldidonna/pk/resources/text/english/EnglishPokemonTextResources.kt
@@ -4,8 +4,7 @@ import com.manueldidonna.pk.resources.text.PokemonTextResources
 
 internal class EnglishPokemonTextResources : PokemonTextResources {
     override val natures: PokemonTextResources.Natures = EnglishNatures()
-
     override val moves: PokemonTextResources.Moves = EnglishMoves()
-
     override val species: PokemonTextResources.Species = EnglishSpecies()
+    override val items: PokemonTextResources.Items = EnglishItems()
 }
