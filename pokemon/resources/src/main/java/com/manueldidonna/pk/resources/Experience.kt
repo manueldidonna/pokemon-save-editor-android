0d7939ec86c4b1754c629668d65d233941243c3b
manueldidonna <manueldidonnadev@gmail.com>
2020-07-10 15:00:34 +0200

    [resources] Add experience groups of Generation II pokemon

--- pokemon/resources/src/main/java/com/manueldidonna/pk/resources/Experience.kt
+++ pokemon/resources/src/main/java/com/manueldidonna/pk/resources/Experience.kt
@@ -1,5 +1,10 @@
 package com.manueldidonna.pk.resources
 
+private const val Fast = 4
+private const val MFast = 0
+private const val MSlow = 3
+private const val Slow = 5
+
 /**
  * [value] is used to find the exp in [ExperiencePointsPerGrowth]
  */
@@ -35,13 +40,12 @@ fun sanitizeExperiencePoints(points: Int, level: Int, experienceGroup: Experienc
     return points
 }
 
-// TODO: support gen 2
 fun getExperienceGroup(speciesId: Int): ExperienceGroup {
-    return when {
-        FastSpecies.find { it == speciesId } != null -> ExperienceGroup.Fast
-        MediumFastSpecies.find { it == speciesId } != null -> ExperienceGroup.MediumFast
-        MediumSlowSpecies.find { it == speciesId } != null -> ExperienceGroup.MediumSlow
-        SlowSpecies.find { it == speciesId } != null -> ExperienceGroup.Slow
+    return when (ExperienceGroupPerSpecies[speciesId - 1]) {
+        MSlow -> ExperienceGroup.MediumSlow
+        MFast -> ExperienceGroup.MediumFast
+        Slow -> ExperienceGroup.Slow
+        Fast -> ExperienceGroup.Fast
         else -> throw IllegalStateException("No Experience group found for species id $speciesId")
     }
 }
@@ -168,24 +172,36 @@ private val ExperiencePointsPerGrowth = intArrayOf(
     1000000, 600000, 1640000, 1059860, 800000, 1250000
 )
 
-
-private val MediumSlowSpecies = intArrayOf(
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 16, 17, 18, 29, 30, 31, 32,
-    33, 34, 43, 44, 45, 60, 61, 62, 63, 64, 65, 66, 67, 68,
-    69, 70, 71, 74, 75, 76, 92, 93, 94, 151
+/**
+ * index: species - 1
+ * value: [ExperienceGroup.value]
+ * @see ExperienceGroup
+ */
+private val ExperienceGroupPerSpecies = intArrayOf(
+    MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MFast,   // 1
+    MFast, MFast, MFast, MFast, MFast, MSlow, MSlow, MSlow, MFast, MFast,   // 2
+    MFast, MFast, MFast, MFast, MFast, MFast, MFast, MFast, MSlow, MSlow,   // 3
+    MSlow, MSlow, MSlow, MSlow, Fast, Fast, MFast, MFast, Fast, Fast,       // 4
+    MFast, MFast, MSlow, MSlow, MSlow, MFast, MFast, MFast, MFast, MFast,   // 5
+    MFast, MFast, MFast, MFast, MFast, MFast, MFast, Slow, Slow, MSlow,     // 6
+    MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow,   // 7
+    MSlow, Slow, Slow, MSlow, MSlow, MSlow, MFast, MFast, MFast, MFast,     // 8
+    MFast, MFast, MFast, MFast, MFast, MFast, MFast, MFast, MFast, Slow,    // 9
+    Slow, MSlow, MSlow, MSlow, MFast, MFast, MFast, MFast, MFast, MFast,    // 10
+    MFast, Slow, Slow, MFast, MFast, MFast, MFast, MFast, MFast, MFast,     // 11
+    Slow, Slow, Fast, MFast, MFast, MFast, MFast, MFast, MFast, Slow,       // 12
+    Slow, MFast, MFast, MFast, MFast, MFast, Slow, Slow, Slow, Slow,        // 13
+    Slow, MFast, MFast, MFast, MFast, MFast, MFast, MFast, MFast, MFast,    // 14
+    MFast, Slow, Slow, Slow, Slow, Slow, Slow, Slow, Slow, Slow,            // 15
+    MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow, MSlow,   // 16
+    MFast, MFast, MFast, MFast, Fast, Fast, Fast, Fast, MFast, Slow,        // 17
+    Slow, MFast, Fast, Fast, Fast, Fast, MFast, MFast, MSlow, MSlow,        // 18
+    MSlow, MSlow, Fast, Fast, MFast, MSlow, MSlow, MSlow, MSlow, Fast,      // 19
+    MSlow, MSlow, MFast, MFast, MFast, MFast, MFast, MSlow, MFast, Fast,    // 20
+    MFast, MFast, MFast, MFast, MFast, MFast, MSlow, MFast, Fast, Fast,     // 21
+    MFast, MFast, MSlow, Slow, MSlow, MFast, MFast, MFast, MFast, Slow,     // 22
+    Slow, Fast, MFast, MFast, Fast, Slow, Slow, Slow, Slow, MFast,          // 23
+    MFast, MFast, MFast, Slow, Fast, MFast, MFast, MFast, MFast, MFast,     // 24
+    Slow, Fast, Slow, Slow, Slow, Slow, Slow, Slow, Slow, Slow,             // 25
+    MSlow                                                                   // 26
 )
-
-private val SlowSpecies = intArrayOf(
-    58, 59, 72, 73, 90, 91, 102, 103, 111, 112, 120, 121, 127, 128,
-    129, 130, 131, 142, 143, 144, 145, 146, 147, 148, 149, 150
-)
-
-private val MediumFastSpecies = intArrayOf(
-    10, 11, 12, 13, 14, 15, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 37, 38,
-    41, 42, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 77, 78, 79, 80,
-    81, 82, 83, 84, 85, 86, 87, 88, 89, 95, 96, 97, 98, 99, 100, 101, 104, 105,
-    106, 107, 108, 109, 110, 114, 115, 116, 117, 118, 119, 122, 123, 124, 125,
-    126, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141
-)
-
-private val FastSpecies = intArrayOf(35, 36, 39, 40, 113)
