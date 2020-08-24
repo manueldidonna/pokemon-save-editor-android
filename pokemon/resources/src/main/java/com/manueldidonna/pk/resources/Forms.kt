1bedb86fa3bb30a1b37852fb0dd47096c9c18b82
manueldidonna <manueldidonnadev@gmail.com>
2020-08-24 19:26:01 +0200

    [pk-resources] Fix unown form validation

--- pokemon/resources/src/main/java/com/manueldidonna/pk/resources/Forms.kt
+++ pokemon/resources/src/main/java/com/manueldidonna/pk/resources/Forms.kt
@@ -3,18 +3,18 @@ package com.manueldidonna.pk.resources
 import com.manueldidonna.pk.core.Version
 import com.manueldidonna.pk.core.generation
 
-private val UnownLetters = ('A'..'Z').toList() + '!'
+private fun unownLetters() = ('A'..'Z').toList()
 
 fun unownLetters(version: Version): List<Char> {
     return when (version.generation) {
         1 -> throw IllegalStateException("Unown doesn't exists in First gen.")
-        2 -> UnownLetters
-        else -> UnownLetters + '?'
+        2 -> unownLetters()
+        else -> unownLetters() + '?' + '!'
     }
 }
 
 fun isValidUnownLetter(letter: Char, version: Version): Boolean {
     if (letter.toUpperCase() in 'A'..'Z') return true
-    if (letter == '!' || (letter == '?' && version.generation >= 3)) return true
+    if (version.generation >= 3 && (letter == '!' || letter == '?')) return true
     return false
 }
